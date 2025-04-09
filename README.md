c3a7cd39f22df298a4e6a9ba57b5e74b794d67cf
admin-xpertia <info.xpertia@xpertia.cl>
2025-04-09 10:26:46 -0400

    feat[back]: update README and enhance API request handling

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # t2-extract
+
+### TAREA:
+1. API - GITHUB --> REST/GRAPHQL
+2. repo_owner, repo_name, #commits, contribuidors.
+fullname(repo_owner/repo_owner)
+3. Filtros 1: null
+Filtro delcodo
--- src/main.py
+++ src/main.py
@@ -6,15 +6,33 @@ load_dotenv()
 token = os.getenv("TOKEN")
 
 def make_request(owner: str, repo: str):
-   req = requests.get(f"https://api.github.com/repos/{owner}/{repo}", headers={"Authorization": f"Bearer {token}"})  
-   if req.status_code == 200:
-       print("Request successful!")
-       print(req.json())
-   else:
-       print(f"Request failed with status code: {req.status_code}")
+   try:   
+      req = requests.get(f"https://api.github.com/repos/{owner}/{repo}", headers={"Authorization": f"Bearer {token}"})  
+      if req.status_code == 200:
+         owner = req.json()["owner"]["login"]
+         print(f"{owner} is the owner of the repository {repo}")
+         return owner
+      else:
+         print(f"Request failed with status code: {req.status_code}")
+   except requests.exceptions.RequestException as e:
+      print(f"An error occurred: {e}")
+
+def get_user_info(owner: str):
+   try:   
+      req = requests.get(f"https://api.github.com/users/{owner}/repos", headers={"Authorization": f"Bearer {token}"})  
+      if req.status_code == 200:
+         print(f"{req.json()}")
+      else:
+         print(f"Request failed with status code: {req.status_code}")
+   except requests.exceptions.RequestException as e:
+      print(f"An error occurred: {e}")
+
+def extract_repository_from_user(owner: str, repo: str):
+   print(f"Extracting repository {repo} from user {owner}", )
 
 def main():
-   make_request("Cucharoth", "t2_eda")
+   owner = make_request("Cucharoth", "t2_eda")
+   get_user_info(owner)
 
    
 if __name__ == "__main__":
