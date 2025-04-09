c1f40210237269a62d4473228ba223b8f7567fb4
admin-xpertia <info.xpertia@xpertia.cl>
2025-04-09 09:56:52 -0400

    feat[back]: extract one endpoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("TOKEN")
+
+def make_request(owner: str, repo: str):
+   req = requests.get(f"https://api.github.com/repos/{owner}/{repo}", headers={"Authorization": f"Bearer {token}"})  
+   if req.status_code == 200:
+       print("Request successful!")
+       print(req.json())
+   else:
+       print(f"Request failed with status code: {req.status_code}")
 
 def main():
-   print("Hello, World!")
+   make_request("Cucharoth", "t2_eda")
+
    
 if __name__ == "__main__":
    main()
